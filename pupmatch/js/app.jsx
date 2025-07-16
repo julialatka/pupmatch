8db2e56a047b5af4840c56e7755dab4a73deeb81
julialatka <j.oszczyna@outlook.com>
2025-07-16 21:17:10 +0200

    Initial commit

---
+++ pupmatch/js/app.jsx
@@ -0,0 +1,18 @@
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+
+// const container = document.getElementById('app');
+// const root = createRoot(container);
+
+// root.render(<h1>PupMatch działa! 🐶</h1>);
+
+
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>PupMatch działa! 🐶</h1>
+    </div>
+  );
+}
---
+++ pupmatch/js/main.jsx
@@ -0,0 +1,13 @@
+import './styles/main.scss';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './app.jsx';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
