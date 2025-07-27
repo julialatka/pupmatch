b4f54da20ce30e7b430d5c516c431be6542e9873
julialatka <julia.latka@bain.com>
2025-07-27 20:49:19 +0200

    styling for pupmatch

--- pupmatch/js/components/Header.jsx
+++ pupmatch/js/components/Header.jsx
@@ -4,13 +4,18 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
        <header className="main-header">
-        <nav>
-            <ul className="nav-list">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/favorites">Favorites</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            </ul>
-        </nav>
+        <div className="header-content">
+            <div className="logo">
+                <span>PUPMATCH</span>
+            </div>
+            <nav>
+                <ul className="nav-list">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/favorites">Favorites</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+                </ul>
+            </nav>
+        </div>
         </header> 
     )
 }
